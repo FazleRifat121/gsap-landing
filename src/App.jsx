a33ae3ea77b5795dfca6d08514251c80ad5d11a4
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-06-25 09:25:38 +0600

    overflow-hidden and art section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
-    <main>
+    <main className="overflow-hidden">
       <Navbar />
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 };
